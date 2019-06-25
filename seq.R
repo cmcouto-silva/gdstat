49165c2f8dcaedd3e1a8b2938933d83ff0470224
cmcouto-silva <cmcouto.silva@gmail.com>
2019-06-25 13:45:50 -0300

    delete useless files

--- seq.R
+++ seq.R
@@ -10,25 +10,22 @@ for(i in c(1:786)) {
 
 any(vec)
 
-from <- seq(1, (length(myseq) - window) + 1, by = 7)
+from <- seq(1, (length(myseq) - window) + 1, by = 5)
 to <- from + (window - 1)
-from;to
-
-exceds <- which(to > myseq[length(myseq)])
-from <- from[-exceds]
-to <- to[-exceds]
 wid <- seq_along(from)
-
-from[5]
-to[5]
-
-
-3 * slide - (slide - 1) 
 
 dt <- data.table(from = from, to = to, wid = wid)
 
+10
+
 window <- 20
 slide <- 7
+
+
+wid[12] * slide - (slide - 1) 
+
+(slide - 1) / slide
+
 
 f <- wid[5] * slide - (slide - 1) 
 t <- f + (window - 1)
