f8c1a22c0b145b1a437cf567d451ab193e84234c
cmcouto-silva <cmcouto.silva@gmail.com>
2020-01-16 18:48:17 -0300

    add optional parameters for ggrepel functions

--- R/mplot.add.genes.R
+++ R/mplot.add.genes.R
@@ -7,17 +7,20 @@
 #' @param data description
 #' @param mplot description
 #' @param gene_col description
+#' @param skip_empty_genes description
 #' @param merge_col description
 #' @param by description
 #' @param label geom_text_repel or geom_label_repel
+#' @param log_pvalue description
+#' @param ... additional parameters for ggrepel functions
 #' 
 #' @return description
 #' 
 #' @import data.table ggplot2
 #' @export
 
-mplot.add.genes <- function(data, mplot, gene_col = "GENE", merge_col = "W_ID", by = "CHR", label = "geom_label_repel",
-                            log_pvalue = 2) {
+mplot.add.genes <- function(data, mplot, gene_col = "GENE", skip_empty_genes = T, merge_col = "W_ID",
+                            by = "CHR", label = "geom_label_repel", log_pvalue = 2, ...) {
   
   # if(!any(grepl(col, colnames(data)))) {
   #   stop("Column name does not match with any column in data.")
@@ -32,20 +35,28 @@ mplot.add.genes <- function(data, mplot, gene_col = "GENE", merge_col = "W_ID", 
   }
   
   y.axis <- as.character(mplot$mapping$y[2])
-  mgplot <- merge(data, mplot$data[, .SD,, .SDcols = c(merge_col, "position")], by = merge_col, sort = F)
-  mgplot <- mgplot[mgplot[, .I[which.max(get(y.axis))], by = by]$V1][LOG_PVALUE >= log_pvalue]
+  datagen <- merge(data, mplot$data[, .SD,, .SDcols = c(merge_col, "position")], by = merge_col, sort = F)
+  
+  if(skip_empty_genes) datagen <- datagen[GENE != ""]
+  data.table::setcolorder(datagen, c("CHR", "POS", "SNP"))
+  
+  datagen <- datagen[datagen[, .I[which.max(get(y.axis))], by = by]$V1][LOG_PVALUE >= log_pvalue]
   
   # Plot genes
   if(label == "geom_text_repel") {
-    mplot +
-      ggrepel::geom_text_repel(data = mgplot, mapping = aes(x = position, y = get(y.axis)),
-                               label = mgplot[, get(gene_col)], size = 2.5,
-                               vjust = 0.8, nudge_x = -0.35)
+    plt <- mplot +
+      ggrepel::geom_text_repel(data = datagen, mapping = aes(x = position, y = get(y.axis)),
+                               label = datagen[, get(gene_col)], size = 2.5,
+                               vjust = 1, nudge_x = 1)
   } else {
-    mplot +
-      ggrepel::geom_label_repel(data = mgplot, mapping = aes(x = position, y = get(y.axis)),
-                               label = mgplot[, get(gene_col)], size = 2.5,
-                               vjust = 0.8, nudge_x = -0.35)
+    plt <- mplot +
+      ggrepel::geom_label_repel(data = datagen, mapping = aes(x = position, y = get(y.axis)),
+                                label = datagen[, get(gene_col)], size = 3,
+                                vjust = 1, nudge_x = 1)
   }
+  
+  plt$datagen <- datagen
+  
+  return(plt)
   
 }
