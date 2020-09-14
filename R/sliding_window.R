18daf6b7f977f7a3f932d1d8bf32681a05cc2dfa
cmcouto-silva <cmcouto.silva@gmail.com>
2020-09-14 11:55:40 -0300

    implement "exact" algo to roll_mean function

--- R/sliding_window.R
+++ R/sliding_window.R
@@ -2,7 +2,7 @@
 
 sliding_window <- function(DT, window_size = 20, step = 5, mode = "fast", col = "PBS", keep.cols = T) {
   
-  rolling_DT <- copy(pbs) #### CHANGEEEEE to DT
+  rolling_DT <- copy(DT)
   rolling_DT[, as.character(col) := frollmean(get(col), window_size, algo = mode, align = "left")]
   rolling_DT <- rolling_DT[seq(1, .N, by = step)][, W_ID := .I][!is.na(get(col))]
   if(isFALSE(keep.cols)) {
