05be25edf9f57feb4311fd9086037ae91f0bf3eb
cmcouto-silva <cmcouto.silva@gmail.com>
2020-10-14 20:31:43 -0300

    fix grouping windows

--- R/exclude_consecutive_windows.R
+++ R/exclude_consecutive_windows.R
@@ -2,17 +2,15 @@
 exclude_consecutive_windows <- function(rolling_DT, col = "LOG_PVALUE", inplace = F) {
   
   rolling_DT <- copy(rolling_DT)
-  window_group <- c(diff(rolling_DT[, W_ID]) != 1)
-  window_group <- rle(window_group)
+  rolling_DT[, c("W_GROUP", "I") := .(character(), .I)]
   
-  window_group_label <- c()
-  for(i in seq_along(window_group$lengths))
-    window_group_label <- append(window_group_label, rep(paste0("W", i), each = window_group$lengths[i]))
+  window_groups <- split(rolling_DT$W_ID, cumsum(c(1, diff(rolling_DT$W_ID) != 1)))
   
-  W <- ifelse(rolling_DT[1, W_ID + 1] != rolling_DT[2, W_ID], "W0", "W1")
-  window_group_label <- c(W, window_group_label)
+  for(i in seq_along(window_groups)) {
+    set(rolling_DT, i = rolling_DT[W_ID %in% window_groups[[i]], I], j = "W_GROUP", value = paste0("WD_", i))
+  }
   
-  rolling_DT[, W_GROUP := window_group_label]
+  rolling_DT[, I := NULL]
   rolling_DT[rolling_DT[, .I[which.max(get(col))], by = W_GROUP]$V1]
   
 }
